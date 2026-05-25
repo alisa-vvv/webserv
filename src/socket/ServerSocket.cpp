3b8bcfa0bb99ba1f7858495d38ff28d5c90cff9f
Tutku Çakır Yılmaz <tutku.cakir123@gmail.com>
2026-05-25 17:48:54 +0200

    added a function inside ServerSocket to close open fd in case of error

--- src/socket/ServerSocket.cpp
+++ src/socket/ServerSocket.cpp
@@ -3,20 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ServerSocket.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tcakir-y <tcakir-y@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tutku <tutku@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 21:31:15 by tutku             #+#    #+#             */
-/*   Updated: 2026/05/12 14:17:30 by tcakir-y         ###   ########.fr       */
+/*   Updated: 2026/05/25 17:20:21 by tutku            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ServerSocket.hpp"
 
+void ServerSocket::closeSocket() //TODO: move to utils
+{
+	if (_fd != -1)
+	{
+		close(_fd);
+		_fd = -1;
+	}
+}
+
 int ServerSocket::setup(void)
 {
-	this->createSocket();
-	this->bindSocket();
-	this->listenSocket();
+	if (this->createSocket() == ERROR)
+	{
+		closeSocket();
+		return ERROR;
+	}
+	if (this->bindSocket() == ERROR)
+	{
+		closeSocket();
+		return ERROR;
+	}
+	if (this->listenSocket() == ERROR)
+	{
+		closeSocket();
+		return ERROR;
+	}
 	return SUCCESS;
 }
 
@@ -34,17 +55,15 @@ int ServerSocket::createSocket()
 		return ERROR;
 	}
 	int flags = fcntl(_fd, F_GETFL, 0);
-	if (flags < 0)
+	if (flags == ERROR)
 	{
 		std::cerr << "Error fcntl F_GETFL: " << std::strerror(errno) << std::endl;
-		close(_fd); //TODO: check
 		return (ERROR);
 	}
 	int status = fcntl(_fd, F_SETFL, flags | O_NONBLOCK);
 	if (status == ERROR)
 	{
 		std::cerr << "Error making the socket non-blocking: " << std::strerror(errno) << std::endl;
-		close(_fd); //TODO: check
 		return ERROR;
 	}
 	std::cout << "Socket created successfully!" << std::endl;
@@ -56,12 +75,11 @@ int ServerSocket::createSocket()
 // https://www.linuxhowtos.org/C_C++/socket.htm
 int ServerSocket::bindSocket()
 {
-	ft_memset(&_address, 0, sizeof(_address));
 	_address.sin_family = AF_INET;
 	_address.sin_port = htons(8080); //TODO: change later when config is ready
 	_address.sin_addr.s_addr = INADDR_ANY; // TODO: later change value, IP address of the host/server
 
-	if (bind(this->get_fd(), (struct sockaddr *)&this->getAddress(), sizeof(this->getAddress())) == ERROR)
+	if (bind(this->get_fd(), (struct sockaddr *)&_address, sizeof(_address)) == ERROR)
 	{
 		std::cerr << "Couldn't bind the port!: " << std::strerror(errno) << std::endl;
 		return ERROR;
@@ -82,26 +100,30 @@ int ServerSocket::listenSocket()
 }
 
 
-ServerSocket::ServerSocket()
+ServerSocket::ServerSocket() : _fd(-1)
 {
+	ft_memset(&_address, 0, sizeof(_address));
 }
 
-ServerSocket &ServerSocket::operator= (const ServerSocket &other)
-{
-	if (this != &other)
-	{
-		this->_fd = other._fd;
-	}
-	return (*this);
-}
+//TODO: check if they would cause double close
+// ServerSocket &ServerSocket::operator= (const ServerSocket &other)
+// {
+// 	if (this != &other)
+// 	{
+// 		this->_fd = other._fd;
+// 		this->_address = other._address;
+// 	}
+// 	return (*this);
+// }
 
-ServerSocket::ServerSocket(const ServerSocket &other)
-{
-	*this = other;
-}
+// ServerSocket::ServerSocket(const ServerSocket &other)
+// {
+// 	*this = other;
+// }
 
 ServerSocket::~ServerSocket()
 {
+	closeSocket();
 }
 
 int ServerSocket::get_fd()
