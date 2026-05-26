07b8486b62bc72f7c063ee2c5272e2db1c19e344
Allyana Alombro <Allygsa@gmail.com>
2026-05-26 21:55:07 +0200

    update: finished http class structure and outlined general recv+request+response structure. added sample req+response files and pseudocode

---
+++ src/http/Http.hpp
@@ -0,0 +1,69 @@
+#ifndef HTTP_HPP
+#define HTTP_HPP
+
+#include <string>
+#include <map>
+
+enum httpType {
+	REQUEST,
+	RESPONSE
+};
+
+enum httpVersion {
+	HTTP_1_0,
+	HTTP_1_1,
+	HTTP_2
+};
+
+std::map<int, std::string> STATUS_MESSAGE = {
+	{200, "OK"},
+	{201, "Created"},
+	{400, "Bad request"},
+	{401, "Unauthorized"},
+	{403, "Forbidden"},
+	{404, "Not Found"},
+	{408, "Request Timeout"},
+	{500, "Internal Server Error"}
+};
+
+enum httpMethod {
+	GET,
+	POST,
+	DELETE,
+	UNKNOWN
+};
+
+class Http {
+	private:
+		httpType							type;
+		httpMethod							method;
+		httpVersion							version;
+		int									statusCode;
+		int									contentLen;
+		bool								hasBody;
+		std::string							uri;
+		std::string							body;
+		std::map<std::string, std::string>	headers;
+
+	public:
+		Http();
+		void			parseRequest(std::string rawString);
+	
+		httpType		getType() const;
+		httpMethod		getMethod() const;
+		int				getContentLen() const;
+		std::string		getBody() const;
+		std::string		getHeader(const std::string &key) const;
+		std::string		getUri() const;
+		httpVersion		getVersion() const;
+		int				getStatusCode() const;
+	
+		void			setResponse(int code); //give statuscode, set header, set body
+		void			setHeader(const std::string &key, const std::string &value);
+		void			setBody(const std::string &body);
+
+		std::string		getResponseString(); //back to raw string for response
+		void			debugPrint();
+};
+
+#endif
