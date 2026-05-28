fdb88c32ff75daead63c24890f353dfa106f56f0
Alisa Valiullina <avaliull@f0r2s8.codam.nl>
2026-05-28 16:19:51 +0200

    update: pulled from main, finished setrequestconfig

---
+++ inc/configParser.hpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                            ::::::::        */
+/*   configParser.hpp                                        :+:    :+:       */
+/*                                                          +:+               */
+/*   By: avaliull <avaliull@student.codam.nl>              +#+                */
+/*                                                        +#+                 */
+/*   Created: 2026/05/28 13:16:53 by avaliull            #+#    #+#           */
+/*   Updated: 2026/05/28 16:17:38 by avaliull            ########   odam.nl   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+# ifndef CLR_NON
+#  define	CLR_NON "\033[0m"
+#  define	CLR_YEL "\033[93m"
+#  define	CLR_CYA "\033[96m"
+#  define	CLR_RED "\033[31m"
+#  define	CLR_GRN "\033[92m"
+#  define	CLR_MAG "\033[95m"
+# endif // CLR_NON and other CLR defines
+//
+#define CONFIG_PATH_DEFAULT "config/default.conf"
+#define CONFIG_PATH_TEST "config/test.conf"
+
+// Config format::
+// name {
+// 	key	value;
+//
+// 	key	value1
+// 		value2
+// 		value3;
+// }
+// etc.
+//
+
+typedef enum e_config_mode {
+	CONFIG_MODE_TEST,
+	CONFIG_MODE_DEFAULT,
+	CONFIG_MODE_PASS,
+}	e_config_mode;
+
+typedef enum e_token_type {
+	BLOCK_NAME, // example: location
+	BLOCK_VAL, // example: /cgi-bin
+	BRACE_OPEN,
+	BRACE_CLOSE,
+	KEY,
+	VALUE,
+	SEMICOLON,
+	NEWLINE,
+} e_token_type;
+
+typedef struct	s_token {
+	e_token_type	token_type;
+	std::string		token_val;
+}	t_config_token;
+
+class	config {
+public:
+private:
+	// int[2]? interface_port_pair;
+	// default errror pages;
+	// maximum size allowed for client request bodies
+	// rules on url route:
+	// 	list of accepted HTTP methods for the rotue;
+	// 	HTTP redirection;
+	// 	Directory where the requested file should be located;
+	// 	enable or disable directory listing;
+	// 	default file to serve when the requested resource is a directory;
+	// 	is uploading authorized
+	// 	extension of cgi
+};
+
+int	parseConfig();
