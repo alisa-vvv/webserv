a264f4496a63c69a17ee73c6e8c2efb0fc035b61
Alisa Valiullina <avaliull@f1r6s17.codam.nl>
2026-06-02 18:31:13 +0200

    added parsing info hpp. contains strings to match with for when creating Config class instance

---
+++ inc/ParsingInfo.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                            ::::::::        */
+/*   ParsingInfo.hpp                                         :+:    :+:       */
+/*                                                          +:+               */
+/*   By: avaliull <avaliull@student.codam.nl>              +#+                */
+/*                                                        +#+                 */
+/*   Created: 2026/06/02 18:15:08 by avaliull            #+#    #+#           */
+/*   Updated: 2026/06/02 18:22:25 by avaliull            ########   odam.nl   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <vector>
+
+class	ParsingInfo {
+public:
+	const std::vector<std::string>	depth0_valid_block_names {
+		"server",
+	};
+	const std::vector<std::string>	server_valid_block_names {
+		"listen",
+		"root",
+		"location",
+		"error_page",
+		"client_max_body_size",
+	};
+	const std::vector<std::string>	location_valid_block_names {
+		"root",
+		"index",
+		"allowed_methods",
+		"upload_store",
+	};
+	const std::vector<std::string>	method_valid_names {
+		"GET",
+		"POST",
+		"DELETE",
+	};
+};
+
--- src/config/config_parser_main.cpp
+++ src/config/config_parser_main.cpp
@@ -6,26 +6,19 @@
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/05/28 14:18:31 by avaliull            #+#    #+#           */
-/*   Updated: 2026/05/28 15:28:12 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/06/02 18:17:42 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "configParser.hpp"
+#include "ParsingInfo.hpp"
 #include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 
-int	exampleUsage(const Config& config) {
-	const char*			host_str = config.servers[0].listen.host;
-	const char* 		port = config.servers[0].listen.port;
-	struct addrinfo*	addrinfo_holder;
-
-	int status = getaddrinfo(host_str, port, NULL, &addrinfo_holder);
-	return (status);
-}
-
 int	main(void) {
+	const	ParsingInfo	parsing_info;
 	parseConfig();
 	Config	newConf;
 
