b9d1477c12b98422cbd727b96cb57dd12bbb49cd
Alisa Valiullina <avaliull@f1r2s5.codam.nl>
2026-06-03 15:59:38 +0200

    moved some func around and started building the scaffolding for token-to-config functions

--- inc/configParsingInfo.hpp
+++ inc/configParsingInfo.hpp
@@ -1,23 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                            ::::::::        */
-/*   ParsingInfo.hpp                                         :+:    :+:       */
+/*   configParsingInfo.hpp                                   :+:    :+:       */
 /*                                                          +:+               */
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/06/02 18:15:08 by avaliull            #+#    #+#           */
-/*   Updated: 2026/06/02 19:48:12 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/06/03 15:45:53 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "configParser.hpp"
 #include <string>
 #include <vector>
+
+// Contains vectors with allowed block names.
+// Each allowed block name corresponds to a function that fills the corresponding
+// values inside a Config instance.
 
 class	ParsingInfo {
 public:
 	const std::vector<std::string>	depth0_valid_block_names {
 		"server",
 	};
+	const std::vector<
+		bool (*)(
+			const ParsingInfo&,
+			const size_t&,
+			const std::vector<t_config_token>&
+	)>	depth_0_matching_functions {
+	};
+
 	const std::vector<std::string>	server_valid_block_names {
 		"listen",
 		"root",
