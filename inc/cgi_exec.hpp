ec320a383f0fa0c39aa31db004620db68c0e46bc
Alisa Valiullina <avaliull@f0r3s9.codam.nl>
2026-07-13 16:12:26 +0200

    added function to create cgi environment. added some of the environment variables. wrote comments for variables that aren't filled as to which information is needed for them

--- inc/cgi_exec.hpp
+++ inc/cgi_exec.hpp
@@ -37,29 +37,6 @@ int	executeCGI(const cfg_server_t& server_config);
 //SERVER_NAME		Your server's fully qualified domain name (e.g. www.cgi101.com)
 //SERVER_PORT		The port number your server is listening on
 //SERVER_SOFTWARE	The server software you're using (e.g. Apache 1.3)
-typedef struct t_envVarCGI {
-	const std::string	cgi_prefix_document_root = "DOCUMENT_ROOT=";
-	const std::string	cgi_prefix_http_cookie = "HTTP_COOKIE=";
-	const std::string	cgi_prefix_http_host = "HTTP_HOST=";
-	const std::string	cgi_prefix_http_referer = "HTTP_REFERER=";
-	const std::string	cgi_prefix_http_user_agent = "HTTP_USER_AGENT=";
-	const std::string	cgi_prefix_https = "HTTPS=";
-	const std::string	cgi_prefix_path = "PATH=";
-	const std::string	cgi_prefix_query_string = "QUERY_STRING=";
-	const std::string	cgi_prefix_remote_addr = "REMOTE_ADDR=";
-	const std::string	cgi_prefix_remote_host = "REMOTE_HOST=";
-	const std::string	cgi_prefix_remote_port = "REMOTE_PORT=";
-	const std::string	cgi_prefix_remote_user = "REMOTE_USER=";
-	const std::string	cgi_prefix_request_method = "REQUEST_METHOD=";
-	const std::string	cgi_prefix_request_uri = "REQUEST_URI=";
-	const std::string	cgi_prefix_script_filename = "SCRIPT_FILENAME=";
-	const std::string	cgi_prefix_script_name = "SCRIPT_NAME=";
-	const std::string	cgi_prefix_server_admin = "SERVER_ADMIN=";
-	const std::string	cgi_prefix_server_name = "SERVER_NAME=";
-	const std::string	cgi_prefix_server_port = "SERVER_PORT=";
-	const std::string	cgi_prefix_server_software = "SERVER_SOFTWARE=";
-} t_envVarCGI;
-
 int	executeCGI(
 	const cfg_server_t& server_config
 );
--- src/cgi_exec/cgi_exec.cpp
+++ src/cgi_exec/cgi_exec.cpp
@@ -34,7 +34,50 @@ static void	cgi_bzero(
 	}
 }
 
+
+	/*	These are things we're most likely not going to have. Putting them here in case */
+//"HTTP_COOKIE=", we don't have cookies
+//"HTTP_HOST=", // all these http related ones seem to be out of scope for us
+//"HTTP_REFERER=",
+//"HTTP_USER_AGENT=",
+//"HTTPS=",
+//"REMOTE_USER=", we don't have authentification so we don't need this
+//"SERVER_ADMIN=", no auth
+
+	/*	These are a maybe, need to check:	*/
+//"REQUEST_URI=", not sure what this is, we might need it?
+//"SCRIPT_FILENAME=", same as above
+
+static char**	constructEnvironment(const cfg_server_t& server_config) {
+	std::vector<std::string>	vars = {
+		"DOCUMENT_ROOT=" + server_config.root, // root directory of the server
+		"PATH=" + (std::string) getenv("PATH"),
+		"QUERY_STRING=", // if method is GET, here we put the query (where is that from?)
+		"REMOTE_ADDR=", // ip of the visitor
+		"REMOTE_HOST=", // host of visitor
+		"REMOTE_PORT=", // port of visitor
+		"REQUEST_METHOD=", // GET or POST
+		"SCRIPT_NAME=", // path to the script we're executing
+		"SERVER_NAME=" + server_config.server_names[0],
+		"SERVER_PORT=", // since we have virtual servers, that one comes from ally's part
+		"SERVER_SOFTWARE=webserv",
+	};
+
+	char**	env = new char*[vars.size() + 1];
+	for (size_t i = 0; i < vars.size(); i++) {
+		const std::string&	cur_string = vars.at(i);
+		env[i] = new char[cur_string.size() + 1];
+		for (size_t j = 0; j < cur_string.size(); j++) {
+			env[i][j] = cur_string.at(j);
+		}
+		env[i][cur_string.size()] = '\0';
+	}
+	env[vars.size()] = NULL;
+	return (env);
+}
+
 static int	findAndExecuteScript(
+	const cfg_server_t& server_config,
 	const std::string binary_name,
 	char *const argv[],
 	std::vector<std::string> paths
@@ -42,7 +85,7 @@ static int	findAndExecuteScript(
 {
 	std::string 	slash_arg;
 	char			path[PATH_MAX];	
-	extern char**	environ;
+	char**			env = constructEnvironment(server_config);
 
 	cgi_bzero(path, PATH_MAX);
 	for (size_t i = 0; i < paths.size(); i++) {
@@ -50,7 +93,7 @@ static int	findAndExecuteScript(
 		for (size_t j = 0; j < slash_arg.size(); j++) {
 			path[j] = slash_arg.at(j);
 		}
-		execve(path, argv, environ);
+		execve(path, argv, env);
 		slash_arg.clear();
 		cgi_bzero(path, PATH_MAX);
 	}
@@ -76,6 +119,7 @@ static std::vector<std::string>	splitPathVar(
 }
 
 static int	tryExecveScript(
+	const cfg_server_t& server_config,
 	const std::string binary_name,
 	char *const argv[]
 )
@@ -84,18 +128,10 @@ static int	tryExecveScript(
 	std::vector<std::string>	path;
 
 	path = splitPathVar();
-	if (path.size() == 0) // add error message about missing path
+	if (path.size() == 0) // add error message about missing path mabe
 		return (1);
-	err_check = findAndExecuteScript(binary_name, argv, path);
+	err_check = findAndExecuteScript(server_config, binary_name, argv, path);
 	return (err_check);
-}
-
-char**	constructEnvironment(const cfg_server_t& server_config) {
-	const t_envVarCGI	env_var_names;
-	char**				env;
-
-	char*	new_env_var;
-	new_env_var = cgi_prefix_document_root + config.
 }
 
 int	executeCGI(
@@ -115,7 +151,7 @@ int	executeCGI(
 	}
 	else if (fork_ret == 0) {
 		std::cout << "executing cgi in child...\n\n";
-		tryExecveScript(PYTHON_EXEC, argv); // add checks for fail to exit out of child
+		tryExecveScript(server_config, PYTHON_EXEC, argv); // add checks for fail to exit out of child
 		std::cout << "if you see this, there's an error\n";
 		exit(1);
 	}
@@ -124,4 +160,3 @@ int	executeCGI(
 	}
 	return (execve_ret);
 }
-
