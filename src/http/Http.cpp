599132e164ccc415d0c192142d17d556b0aad914
Ally Alombro <aalombro@f1r1s21.codam.nl>
2026-07-20 10:28:59 +0200

    update: removed timer.hpp; fixed httptester errors

--- src/http/Http.cpp
+++ src/http/Http.cpp
@@ -137,40 +137,40 @@ void Http::debugPrintRequest()
 
 void Http::debugPrintRequestConfig()
 {
-	cfg_server_t server = requestConfig->server;
-	t_location location = requestConfig->location;
+	const cfg_server_t *server = requestConfig.server;
+	const t_location *location = requestConfig.location;
 	size_t serverNameCount = server->server_names.size();
 	std::cout << "=== Context Request Config ====" << std::endl;
 	std::cout << "============ SERVER ===========" << std::endl;
 	std::cout << std::endl << "===============================" << std::endl;
 	for (size_t i = 0; i < serverNameCount; i++)
-		std::cout << "Server names: " << this->requestConfig.server->server_names[i] << std::endl;
-	std::cout << "IP Address: " << server.ip_addr << std::endl;
-	for (size_t i = 0; i < server.ports.size(); i++)
-		std::cout << "Port: " << server.ports[i] << std::endl;
-	std::cout << "Root: " << server.root << std::endl;
-	std::cout << "Client max body: " << server.client_max_body_size << std::endl;
-	std::cout << "Autoindex: " << server.autoindex << std::endl;
-	for (const auto& [code, link] : server.error_pages)
+		std::cout << "Server names: " << server->server_names[i] << std::endl;
+	std::cout << "IP Address: " << server->ip_addr << std::endl;
+	for (size_t i = 0; i < server->ports.size(); i++)
+		std::cout << "Port: " << server->ports[i] << std::endl;
+	std::cout << "Root: " << server->root << std::endl;
+	std::cout << "Client max body: " << server->client_max_body_size << std::endl;
+	std::cout << "Autoindex: " << server->autoindex << std::endl;
+	for (const auto& [code, link] : server->error_pages)
 		std::cout << "Error page status: " << code << std::endl << "Error page link: " << link << std::endl;
 	std::cout << std::endl << "===============================" << std::endl << std::endl;
 
 	std::cout << "============ LOCATION ===========" << std::endl;
 
-	std::cout << "Prefix: " << location.prefix << std::endl;
-	std::cout << "Root: " << location.root << std::endl;
-	std::cout << "Index: " << location.index << std::endl;
-	std::cout << "Autoindex: " << location.autoindex << std::endl;
+	std::cout << "Prefix: " << location->prefix << std::endl;
+	std::cout << "Root: " << location->root << std::endl;
+	std::cout << "Index: " << location->index << std::endl;
+	std::cout << "Autoindex: " << location->autoindex << std::endl;
 	
-	for (const auto& [method, allowed] : location.allowed_methods)
+	for (const auto& [method, allowed] : location->allowed_methods)
 		std::cout << "Method: " << method << ", Allowed: " << allowed << std::endl;
 	
-	std::cout << "Upload store: " << location.upload_store << std::endl;
-	std::cout << "CGI Pass path: " << location.cgi_pass.path << std::endl;
-	std::cout << "CGI Pass extension: " << location.cgi_pass.extension << std::endl;
-	std::cout << "Return code: " << location.returns.code << std::endl;
-	if (location.returns.code)
-		std::cout << "Return target: " << location.returns.target << std::endl;
+	std::cout << "Upload store: " << location->upload_store << std::endl;
+	std::cout << "CGI Pass path: " << location->cgi_pass.path << std::endl;
+	std::cout << "CGI Pass extension: " << location->cgi_pass.extension << std::endl;
+	std::cout << "Return code: " << location->returns.code << std::endl;
+	if (location->returns.code)
+		std::cout << "Return target: " << location->returns.target << std::endl;
 }
 
 
@@ -181,12 +181,12 @@ void Http::debugPrintHttpClassAttributes()
 	std::cout << "ContentLen: " << getContentLen() << std::endl;
 	std::cout << "==Headers==" <<std::endl;
 	for (const auto& [header, content] : _requestHeaders)
-		std::cout << header ": " << content << std::endl;
+		std::cout << header << ": " << content << std::endl;
 	std::cout << "Body: " << getBody() << std::endl;
 	std::cout << "Version: " << getVersion() << std::endl;
 	std::cout << "Status code: " << getStatusCode() << std::endl;
 	std::cout << "Extension: " << getExtension() << std::endl;
 	std::cout << "ReceivedUri: " << getReceivedUri() << std::endl;
 	std::cout << "BuiltUri: " << getBuiltUri() << std::endl;
-	std::cout << "Content type: " << getContentTypeExtension() << std::endl;
+	std::cout << "Content type: " << getContentTypeExtension(getHeader("Content-Type")) << std::endl;
 }
