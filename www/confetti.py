85e4b660a51efe84c379225de83683b9592feb14
Tutku Cakir Yilmaz <tcakir-y@f1r1s9.codam.nl>
2026-07-22 12:59:21 +0200

    finished the upload button

--- www/confetti.py
+++ www/confetti.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-print("Confetti requested")
+
+print("Content-Type: text/html")
+print()
+
+print("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload Result</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Upload received!</h1>
+    <p>Your Python CGI script ran successfully.</p>
+</body>
+</html>
+""")
--- www/upload.js
+++ www/upload.js
@@ -5,9 +5,8 @@ const fileNameElement = document.getElementById("fileName");
 fileUploader.addEventListener("change", function ()
 {
 	const file = this.files[0];
-	submitButton.hidden = !file;
 	
-	if (fileNameElement.textContent = file) {
+	if (file) {
 		fileNameElement.textContent = "Selected file: " + file.name;
 		submitButton.hidden = false;
 	} else {
@@ -15,3 +14,35 @@ fileUploader.addEventListener("change", function ()
 		submitButton.hidden = true;
 	}
 });
+
+function submitWithoutReload(event)
+{
+	event.preventDefault(); //browser’s form submission event
+
+	const form = document.getElementById("uploadForm");
+	const uploadMessage = document.getElementById("uploadMessage");
+	const formData = new FormData(form);
+
+	fetch(form.action, {
+		method: "POST",
+		body: formData
+	})
+	.then(response => {
+		if (!response.ok)
+			throw new Error(`Upload failed: HTTP ${response.status}`);
+
+		return response.text();
+	})
+	.then(text => {
+		console.log(text);
+		uploadMessage.textContent = text;
+	}) //reads the response body and converts it into a JavaScript string
+	.catch(error => {
+		console.error(error);
+		uploadMessage.textContent = "Upload failed! gg";
+	});
+}
+
+//TODO: after clicking upload button, hide that button and instead show 
+// 	1. upload successfull/ upload failed message (needs cgi part)
+// 	2. a new button that says "Upload new file" to start over
