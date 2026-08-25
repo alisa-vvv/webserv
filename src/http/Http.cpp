14cb6de3eeb8da8f37a2aeafb28e3c2cff825f3f
Ally Alombro <aalombro@f1r2s13.codam.nl>
2026-08-25 15:56:18 +0200

    added debugmode on http, fixed error pages wrong link

--- src/http/Http.cpp
+++ src/http/Http.cpp
@@ -233,9 +233,11 @@ void Http::debugPrintHttpClassAttributes()
 /// @param isVar 
 void Http::printError(std::string error, std::string functName, bool isVar)
 {
-	if (isVar)
-		std::cout << RED << "Printing variable " << error << ": " << RESET << functName << std::endl;
-	else 
-		std::cout << RED << "Error: " << error << " on function " << RESET << functName << std::endl;
+	if (DEBUG_HTTP) {
+		if (isVar)
+			std::cout << RED << "Printing variable " << error << ": " << RESET << functName << std::endl;
+		else 
+			std::cout << RED << "Error: " << error << " on function " << RESET << functName << std::endl;
+	}
 }
 
