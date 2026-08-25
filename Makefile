efab2c173123e6a7610ce16f5758946afae2cb91
Alisa Valiullina <avaliull@f1r2s9.codam.nl>
2026-08-25 17:47:13 +0200

    added tester stuff

--- Makefile
+++ Makefile
@@ -167,5 +167,7 @@ leak:
 val:
 	valgrind -s --track-fds=yes --track-origins=yes \
 	--leak-check=full --show-leak-kinds=all ./$(NAME) $(INPUT)
+youpi:
+	./$(NAME) config/youpi
 
 .PHONY:	clangd all clean fclean re test run leak debug gdb
--- inc/utils.hpp
+++ inc/utils.hpp
@@ -6,14 +6,11 @@
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/08/25 17:14:36 by avaliull            #+#    #+#           */
-/*   Updated: 2026/08/25 17:17:40 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/08/25 17:45:43 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
-//#include "Client.hpp"
-//#include "Http.hpp"
 
 class Client;
 class Http;
